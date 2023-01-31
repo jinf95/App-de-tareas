20e64cc85b3396c2089c5d6f2d40b71d9a70dede
Jinf95 <jinf95n@gmail.com>
2023-01-31 19:19:50 -0300

    Se agrego paleta de colores y se estructuraron componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
 
 function App() {
-  
-
   return (
     <div className="App">
+      <Navbar/>
       <h1>Task Manager</h1>
+      <Table/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
